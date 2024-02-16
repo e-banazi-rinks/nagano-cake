26353e0379c16cad46980e5f8b008a74484505e4
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-02-16 12:39:17 +0000

    [Making]view/public/top

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -9,6 +9,6 @@ class Public::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :is_active)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
   end 
 end
