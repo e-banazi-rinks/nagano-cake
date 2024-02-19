0f23e60d96e1cdd3df4f5982ef45552101b75f86
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-02-19 07:56:53 +0000

    [Add]customer/edit_function

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,31 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to request.referer, notice: "You have created book successfully."
+    else
+      @addresses = Address.all
+      render 'index'
+    end
+
+  end
+
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,20 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  
   def show
     @customer = current_customer
   end
 
   def edit
+  end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
   
   def unsubscribe
@@ -16,4 +27,11 @@ class Public::CustomersController < ApplicationController
     reset_session
     redirect_to root_path
   end
+end
+
+
+private
+
+def customer_params
+  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :email)
 end
