03c73765a78a48b0642bee7b3857d56ddd3ceec3
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-02-24 08:56:45 +0000

    [Fix]items-show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.page(params[:page])
   end
@@ -11,18 +11,21 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
-    customer = Customer.find(params[:id])
-    customer.update(customer_params)
-    redirect_to admin_customer_path(customer.id)
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
   end
-  
+
   private
-  
+
   def customer_params
-    params.require(:customer).permit(:name, :email, :is_active)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telehone_number, :email, :is_active)
   end
-  
+
 end
 
