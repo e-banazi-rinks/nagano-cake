5fb22e8f1e58fd06440389154134485d9ad52960
aknagano <150144545+aknagano@users.noreply.github.com>
2024-02-25 02:14:46 +0000

    layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,11 +6,16 @@ class Admin::OrdersController < ApplicationController
   end
   
   def update
-      @order = Order.find(params[:id])
-    if @order.update(order_params)
-      redirect_to admin_order_path(@order)
+      order = Order.find(params[:id])
+    if order.update(order_params)
+      redirect_to admin_root_path
     else
       render :show
     end  
   end
+  
+  private
+    def order_params
+      params.require(:order).permit(:order_status)
+    end
 end
