a0e44b22fb254f609393bc821fa611b01e9e0541
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-02-26 11:18:56 +0000

    [fix]order_status,order_show

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,24 +1,22 @@
 class Admin::OrderDetailsController < ApplicationController
 before_action :authenticate_admin!
-  def update 
+  def update
     order_detail = OrderDetail.find(params[:id])
     order = order_detail.order
     order_details = order.order_details.all
    #@order_detail.update(order_detail_params)
-    
    is_updated = true
     if order_detail.update(order_detail_params)
        order.update(order_status: "making") if order_detail.making_status == "in_making"
-       order_details.each do |order_detail| 
+       order_details.each do |order_detail|
           if order_detail.making_status != "making_completed"
-            is_updated = false 
+            is_updated = false
           end
        end
        order.update(order_status: "preparing_ship") if is_updated
     end
        redirect_to admin_order_path(order.id)
   end
-  
   private
     def order_detail_params
       params.require(:order_detail).permit(:making_status)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :genre
 
-  
+
   has_many :cart_items, dependent: :destroy
   has_many :customers, through: :cart_items
   has_many :order_details
-  
+
 
   has_one_attached :image
   enum status: { on_sale: 0, off_sale: 1 }
 
   validates :name,presence:true
   validates :price,presence:true
-  
+
   def get_profile_image(width, height)
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   #消費税を求めるメソッド
   def with_tax_price
     (price * 1.1).floor
   end
-  
+
 end
